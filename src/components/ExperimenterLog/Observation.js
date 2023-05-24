22957efbfd0f13a2137aee5f2f9286e8c66489aa
tristanzucker <tristanzucker@gmail.com>
2023-05-24 16:46:43 +0100

    Show observation form next to every observation

--- src/components/ExperimenterLog/Observation.js
+++ src/components/ExperimenterLog/Observation.js
@@ -13,27 +13,42 @@ const Observation = ({public_user_id, observation}) => {
     const sanitized_observation_prompt = DOMPurify.sanitize(observation.observation_prompt);
     const sanitized_observation = DOMPurify.sanitize(observation.observation);
 
-    // If there there is no observation for an observation prompt, don't display anything other than the experiment
-    if (sanitized_observation === "") {
-        return (<div></div>)
+    // If there is an observation, display the observation prompt and observation and observation input
+    if (sanitized_observation !== "") {
+        return (
+            <div>
+                <div class="p-0.5"></div>
+                <p class="my-2 text-l tracking-tight font-bold text-gray-500 md:text-xl">
+                    <span dangerouslySetInnerHTML={{__html: sanitized_observation_prompt}} />
+                </p>
+                <p class="my-2 text-l tracking-tight text-gray-500 md:text-xl">
+                    <span dangerouslySetInnerHTML={{__html: sanitized_observation}} />
+                </p>
+                <ObservationForm 
+                    public_user_id={public_user_id}
+                    observation_prompt_id={observation.observation_prompt_id} />
+            </div>
+        )
     }
 
-    // If there is an observation, display the observation prompt and observation
-    return (
-        <div>
-            <div class="p-0.5"></div>
-            <p class="my-2 text-l tracking-tight font-bold text-gray-500 md:text-xl">
-                <span dangerouslySetInnerHTML={{__html: sanitized_observation_prompt}} />
-            </p>
-            <p class="my-2 text-l tracking-tight text-gray-500 md:text-xl">
-                <span dangerouslySetInnerHTML={{__html: sanitized_observation}} />
-            </p>
-            <ObservationForm 
-                public_user_id={public_user_id}
-                observation_prompt_id={observation.observation_prompt_id} />
-        </div>
-        
-    )
+    // If there is no observation but there is an observation prompt, display the observation prompt and observation input
+    else if (sanitized_observation_prompt !== "") {
+        return (
+            <div>
+                <div class="p-0.5"></div>
+                <p class="my-2 text-l tracking-tight font-bold text-gray-500 md:text-xl">
+                    <span dangerouslySetInnerHTML={{__html: sanitized_observation_prompt}} />
+                </p>
+                <ObservationForm 
+                    public_user_id={public_user_id}
+                    observation_prompt_id={observation.observation_prompt_id} />
+            </div>
+        )
+    }
+
+    // If there there is no observation prompt or observation, don't display anything other than the experiment
+    return (<div></div>)
+
 }
 
 export { Observation }
